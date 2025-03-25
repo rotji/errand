b428cc63d6b7fae632276a7495e11bf9efeef7d5
rotji <starrotji@gmail.com>
2025-03-25 01:55:25 +0100

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <div className={styles.about}>
       <h1 className={styles.title}>About Errand </h1>
       <p className={styles.description}>
-        This platform connects you with verified agents for errands.are you looking for somebody to send for an errand, are you looking for somebody to pick or delivered your goods from one location to another, are you looking for somebody to pick or drop your child at school, are you looking for somebody to wash your cars, are you looking for somebody to clean your house, are you looking for somebody to go and stand in a line for you in banks or elsewhere, do you have menial/big tasks that you want somebody to joint you, if your answer is yes to those questions then this platform is for you.
+        This platform connects you with verified agents for errands.are you looking for somebody to send for an errand, are you looking for somebody to pick or delivered your goods from one location to another, are you looking for somebody to pick or drop your child at school, are you looking for somebody to wash your cars, are you looking for somebody to clean your house, are you looking for somebody to go and stand in a line for you in banks or elsewhere, are you a business owner and you want to make your high value customers convenience a priority, you can also request the services of professional services like looking for services of dentists, plumbers, electricians, etc, do you have menial/big tasks that you want somebody to joint you,  if your answer is yes to those questions then this platform is for you.
       </p>
     </div>
   );
