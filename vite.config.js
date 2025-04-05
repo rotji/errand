4da3acbe54d15005a8641c665fdf1ca5625f9feb
rotji <starrotji@gmail.com>
2025-04-05 05:21:40 +0100

    minor adjust

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfigcd({
-  base: './', // ✅ Add this line
-  plugins: [reactC()],
+export default defineConfig({
+  base: './', // ✅ This helps with relative paths on Netlify
+  plugins: [react()],
 })
